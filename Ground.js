fae43ced8952fb044a8983237e686c2fd8f4f56b
ASTHARANJAN <74598712+ASTHARANJAN@users.noreply.github.com>
2021-01-13 16:25:52 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,17 +1,20 @@
 class Ground {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
+    constructor( x, y, width, height ){
+        var options = {
+            isStatic : true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        World.add( world, this.body );
+
     }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("white");
-      rect(pos.x, pos.y, this.width, this.height);
+    display (){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("pink");
+        rect(pos.x,pos.y,this.width,this.height);
     }
-  };
+};
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# PLINKO
-A satisfying simulation of balls falling down. You can adjust the values if you want
+# TowerSiege-1
